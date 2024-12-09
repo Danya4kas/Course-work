bd81f4f7593eeaf0d13692cf1de6a3025e98c482
Danya4kas <daniilmosiychuk007@gmail.com>
2024-12-09 23:28:58 +0200

    Add files via upload

--- Course_work/analytics_page/js/script.js
+++ Course_work/analytics_page/js/script.js
@@ -64,3 +64,67 @@ function changeTheme() {
 }
 
 
+
+  function parsePrice(price) {
+    return parseFloat(price.replace(' грн', '').replace(',', '.'));
+  }
+
+
+  function parseDate(date) {
+    let [day, month, year] = date.split('.');
+    return new Date(year, month - 1, day);
+  }
+
+  function parsePrice(price) {
+    return parseFloat(price.replace(/\s|грн/g, '').replace(',', '.'));
+  }
+
+
+  function parseDate(date) {
+    let [day, month, year] = date.split('.');
+    return new Date(year, month - 1, day);
+  }
+
+
+  function sort() {
+    const sortPrice = document.querySelectorAll('.sort select')[0];
+    const sortDate = document.querySelectorAll('.sort select')[1];
+    const sortQuantity = document.querySelectorAll('.sort select')[2];
+
+    const table = document.getElementById('droneTable');
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+
+ 
+    const priceSortValue = sortPrice.value;
+    const dateSortValue = sortDate.value;
+    const quantitySortValue = sortQuantity.value;
+
+
+    if (priceSortValue === 'week') {
+      rows.sort((a, b) => parsePrice(a.cells[3].textContent) - parsePrice(b.cells[3].textContent));
+    } else if (priceSortValue === 'month') {
+      rows.sort((a, b) => parsePrice(b.cells[3].textContent) - parsePrice(a.cells[3].textContent));
+    } else if (priceSortValue === 'year') {
+      rows.sort((a, b) => parsePrice(a.cells[3].textContent) - parsePrice(b.cells[3].textContent));
+    }
+
+
+    if (dateSortValue === 'week') {
+      rows.sort((a, b) => parseDate(a.cells[1].textContent) - parseDate(b.cells[1].textContent));
+    } else if (dateSortValue === 'month') {
+      rows.sort((a, b) => parseDate(b.cells[1].textContent) - parseDate(a.cells[1].textContent));
+    } else if (dateSortValue === 'year') {
+      rows.sort((a, b) => parseDate(a.cells[1].textContent) - parseDate(b.cells[1].textContent));
+    }
+
+    
+    if (quantitySortValue === 'week') {
+      rows.sort((a, b) => a.cells[4].textContent - b.cells[4].textContent);
+    } else if (quantitySortValue === 'month') {
+      rows.sort((a, b) => b.cells[4].textContent - a.cells[4].textContent);
+    } else if (quantitySortValue === 'year') {
+      rows.sort((a, b) => a.cells[4].textContent - b.cells[4].textContent);
+    }
+
+    rows.forEach(row => table.querySelector('tbody').appendChild(row));
+  }
